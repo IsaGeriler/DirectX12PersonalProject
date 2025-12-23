da81a7542cbf0c7195e720ae6ca1eeda54e454d8
Isa Berk Geriler <gerilerisaberk@gmail.com>
2025-12-23 15:36:59 +0000

    Blue screen was drawn to the window.

--- DirectX12PersonalProject/main.cpp
+++ DirectX12PersonalProject/main.cpp
@@ -1,10 +1,23 @@
+#include "DX12Core.h"
 #include "Window.h"
+
+extern "C" {
+	_declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+}
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
 	Window win;
 	win.create("My Window", 1024, 1024);
+
+	DX12Core core;
+	core.init(win.hwnd, 1024, 1024);
 	
 	while (true) {
+		core.beginFrame();
 		win.processMessages();
+		if (win.keys[VK_ESCAPE] == 1) break;
+		core.finishFrame();
 	}
+	core.flushGraphicsQueue();
+	return 0;
 }
